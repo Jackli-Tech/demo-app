38956f124e82f491e48dc5109628af9f29359f4f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 05:22:58 +0000

    Deploy Jackli-Tech/demo-app to github.com/Jackli-Tech/demo-app.git:gh-pages

---
+++ precache-manifest.b8ffc6233d9d25c2980369c2ad94a096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c75dfeaaa8956ecb63",
+    "url": "/demo-app/static/css/main.3fe45041.chunk.css"
+  },
+  {
+    "revision": "b7c75dfeaaa8956ecb63",
+    "url": "/demo-app/static/js/main.b7c75dfe.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d4e39eb5e23a0f89cc779a1833da49a",
+    "url": "/demo-app/index.html"
+  }
+];
